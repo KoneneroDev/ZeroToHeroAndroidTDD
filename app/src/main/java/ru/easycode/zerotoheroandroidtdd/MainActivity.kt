6c238f8c589ab5a1ff2b3550c80b781b80a10208
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2024-07-06 23:05:23 +0300

    feat: solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,20 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.changeButton)
+        button.setOnClickListener {
+            val textView = findViewById<TextView>(R.id.titleTextView)
+            textView.setText("I am an Android Developer!")
+        }
     }
 }
