1e44972163130f003150df2dd25f3f6bd4bf8bd2
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2024-07-09 22:33:56 +0300

    feat: solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,36 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.rootLayout)
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.removeButton)
+
+        button.setOnClickListener{
+            linearLayout.removeView(textView)
+            button.isEnabled = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("BUTTON", button.isEnabled)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        linearLayout.removeView(textView)
+        button.isEnabled = savedInstanceState.getBoolean("BUTTON")
     }
 }
