26e26c239e5251a45f1f8c116b4f96265406d899
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2024-07-10 00:18:54 +0300

    feat: solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,37 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var count: Count.Base = Count.Base(2)
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.countTextView)
+        button = findViewById(R.id.incrementButton)
+
+        button.setOnClickListener{
+            textView.text = count.increment(textView.text.toString())
+        }
+    }
+}
+
+interface Count {
+
+    fun increment(number: String) : String
+
+    class Base(private var step: Int) : Count {
+
+        init {
+            check(step > 0) { "step should be positive, but was $step" }
+        }
+
+        override fun increment(number: String) = number.toInt().plus(step).toString()
+
     }
 }
