e984ed44afd6ea682808daf7904bfef47726cc9d
Svyatoslav Kochergin <SKochergin@alfabank.ru>
2024-07-13 16:47:50 +0300

    feat: solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,45 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private lateinit var button: Button
+    private lateinit var progressBar: ProgressBar
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        button = findViewById(R.id.actionButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            coroutineScope.launch {
+                delay(3500)
+                progressBar.visibility = View.INVISIBLE
+                textView.visibility = View.VISIBLE
+                button.isEnabled = true
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
     }
 }
